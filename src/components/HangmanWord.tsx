347afe9624b15ad216345d1ff04a0a84c1243d9d
Jared Siemen <siemenjm@gmail.com>
2022-12-07 21:44:32 -0500

    Add word visibility/hidden functionality to HangmanWord

--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -1,3 +1,29 @@
 export function HangmanWord() {
+    const word = 'test';
+    const guessedLetters = ['t', 'e'];
     
+    return (
+        <div style={{
+            display: 'flex',
+            gap: '.25em',
+            fontSize: '6rem',
+            fontWeight: 'bold',
+            textTransform: 'uppercase',
+            fontFamily: 'monospace'
+        }}>
+            {word.split('').map((letter, index) => {
+                return (
+                    <span style={{ borderBottom: '.1em solid black' }} key={index}>
+                        <span style={{ 
+                            visibility: guessedLetters.includes(letter)
+                            ? 'visible'
+                            : 'hidden'
+                         }}>
+                            {letter}
+                         </span>
+                    </span>
+                );
+            })}
+        </div>
+    );
 }
