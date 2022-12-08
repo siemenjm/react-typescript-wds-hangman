e88ef1fdfa9f40bfd878c5e4ecdc4353a55098e0
Jared Siemen <siemenjm@gmail.com>
2022-12-07 21:10:54 -0500

    Add wordToGuess and guessedLetters state

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,14 @@
+import { useState } from "react";
+import words from './wordList.json';
+
 function App() {
+    const [wordToGuess, setWordToGuess] = useState(() => {
+        return words[Math.floor(Math.random() * words.length)];
+    });
+    const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
+    console.log(wordToGuess);
+
     return <h1>Hi</h1>
 }
 
